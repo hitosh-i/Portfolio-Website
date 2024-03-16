2c2c77c27d3412577a88df096f0a891652427833
hitoshi-i <abdoulmbarry456@gmail.com>
2024-03-16 06:49:52 -0400

    Wokring on NavBar padding

--- src/components/NavBar/navbar.js
+++ src/components/NavBar/navbar.js
@@ -1,11 +1,20 @@
 import React from 'react'
 import './navbar.css'
+import logo from '../../assets/logothreeNew.png'
+import {Link} from 'react-scroll';
 
 const Navbar = () => {
     return (
         <nav className="navbar">
-            <img src = "" alt=""/>
-            .desktop
+            <img src = {logo} alt="Logo" className='logo'/>
+            <div className="desktopMenu">
+                <Link className="desktopMenuListItem">Home</Link>
+                <Link className="desktopMenuListItem">About</Link>
+                <Link className="desktopMenuListItem">Portfolio</Link>
+                <Link className="desktopMenuListItem">Clients</Link>
+            </div>
+            <button className="desktopMenuButton">
+                <img src="" alt="" className="desktopMenuImg" />Contact Me Here</button>
         </nav>
     )
 }
