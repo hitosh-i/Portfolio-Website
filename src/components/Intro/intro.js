79d96d7c1196b4f038a1f6e9b1c51da89af18baa
hitosh-i <abdoulmbarry456@gmail.com>
2024-03-20 12:49:13 -0400

    Trying to edit in huge contact photo and edit button style in css

--- src/components/Intro/intro.js
+++ src/components/Intro/intro.js
@@ -1,9 +1,20 @@
 import React from "react";
 import './intro.css';
+import bg from '../../assets/image.png';
+import btnImg from '../../assets/hireme.png'
+import {Link} from 'react-scroll';
 
 const Intro = () => {
     return(
-        <div>intro</div>
+        <section id="intro">
+            <div className="introContent">
+            <spam className="hello">Hello,</spam>
+            <span className="introText">I'm <span className="Abdoul"> Abdoul</span><br />Kennesaw State University Student</span>
+            <p className="introPara">I'm a computer science student currently attending Kennesaw State University and looking for experiences!</p>
+            <Link><button className="btn"><img src={btnImg} alt="Hire Me" />Hire Me</button></Link>
+            </div>
+            <img src="{bg}" alt="Profile" className="bg" />
+        </section>
     )
 }
 
