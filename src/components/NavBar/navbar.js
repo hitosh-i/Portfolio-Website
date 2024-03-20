d9ec8977ed1f8ece57fd5810068ea6e5e2961c9a
hitoshi-i <abdoulmbarry456@gmail.com>
2024-03-20 03:51:53 -0400

    Navigation Bar has been made!

--- src/components/NavBar/navbar.js
+++ src/components/NavBar/navbar.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import './navbar.css'
 import logo from '../../assets/logothreeNew.png'
-import contacting from '../../assets/contact.png'
+import contacting from '../../assets/contactmetwo.png'
 import {Link} from 'react-scroll';
 
 const Navbar = () => {
