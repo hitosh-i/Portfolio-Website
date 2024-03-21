618e6809f7f49e5cf90a7b6ba61d81aa64a10a34
hitoshi-i <abdoulmbarry456@gmail.com>
2024-03-21 06:04:40 -0400

    Added the correct photo in and cut attemped at cutting myself out

--- src/components/Intro/intro.js
+++ src/components/Intro/intro.js
@@ -1,7 +1,7 @@
 import React from "react";
 import './intro.css';
-import bg from '../../assets/image.png';
-import btnImg from '../../assets/hireme.png'
+import bg from '../../assets/meCutOutSmall.png';
+import btnImg from '../../assets/cutOutBetter.png'
 import {Link} from 'react-scroll';
 
 const Intro = () => {
@@ -10,10 +10,10 @@ const Intro = () => {
             <div className="introContent">
             <spam className="hello">Hello,</spam>
             <span className="introText">I'm <span className="Abdoul"> Abdoul</span><br />Kennesaw State University Student</span>
-            <p className="introPara">I'm a computer science student currently attending Kennesaw State University and looking for experiences!</p>
-            <Link><button className="btn"><img src={btnImg} alt="Hire Me" />Hire Me</button></Link>
+            <p className="introPara">I'm a computer science student currently attending <br />Kennesaw State University and looking for experiences!</p>
+            <Link><button className="btn"><img src={btnImg} alt="Hire Me" className="btnImg"/>Hire Me</button></Link>
             </div>
-            <img src="{bg}" alt="Profile" className="bg" />
+            <img src={bg} alt="Profile" className="bg" />
         </section>
     )
 }
