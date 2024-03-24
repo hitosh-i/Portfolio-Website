0b644b448cb4102a3feb649495a783200c626cb4
hitosh-i <abdoulmbarry456@gmail.com>
2024-03-24 03:18:28 -0400

    Added logos and contact me section at bottom of page; Not footer yet on webage

--- src/components/Contact/contact.js
+++ src/components/Contact/contact.js
@@ -1,7 +1,11 @@
 import React from "react";
 import './contact.css';
 import Github from '../../assets/githubwideSized.png'
-import LinkedIn from '../../assets/linkedInWideSized.png'
+import LinkedIn from '../../assets/linkedInRight.png'
+import Youtube from '../../assets/youtube.png'
+import Twitter from '../../assets/twitter.png'
+import Facebook from '../../assets/facebook-icon.png'
+import Instagram from '../../assets/instagram.png'
 
 const Contact = () => {
     return (
@@ -15,8 +19,21 @@ const Contact = () => {
                 <img src={Github} alt="contactApps" className="clientImg" />
                 <img src={LinkedIn} alt="contactApps" className="clientImg" />
             </div>
-            <div className="contactMe">
-
+            <div id="contact">
+                <h1 className="contactPagetitle">Contact Me</h1>
+                <span className="contactDesc">You can fill out the form below to discuss and work opportunities.</span>
+                <form className="contactForm">
+                    <input type="text" className="name" placeholder="Your Name"/>
+                    <input type="text" className="email" placeholder="Your Email"/>
+                    <textarea type="text" className="message" rows="5" placeholder="Your Message"/>
+                    <button type="submit" value='Send' className="submitBtn">Submit</button>
+                    <div className="links">
+                        <img src={Youtube} alt="" className="link"/>
+                        <img src={Twitter} alt="" className="link"/>
+                        <img src={Facebook} alt="" className="link"/>
+                        <img src={Instagram} alt="" className="link"/>
+                    </div>
+                </form>
             </div>
         </section>
     )
