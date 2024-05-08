f38fce9616179239bf40a30c411e7225ca0d792a
hitoshi-i <abdoulmbarry456@gmail.com>
2024-05-07 22:02:00 -0400

    Added a bunch of new things lol

--- src/components/NavBar/navbar.js
+++ src/components/NavBar/navbar.js
@@ -1,10 +1,12 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './navbar.css'
 import logo from '../../assets/logothreeNew.png'
 import contacting from '../../assets/contactmetwo.png'
 import {Link} from 'react-scroll';
+import menu from '../../assets/menutwo.png'
 
 const Navbar = () => {
+    const [showMenu, setShowMenu] = useState(false);
     return (
         <nav className="navbar">
             <img src = {logo} alt="Logo" className='logo'/>
@@ -17,7 +19,13 @@ const Navbar = () => {
             <button className="desktopMenuButton">
                 <img src={contacting} alt="" className="desktopMenuImg" />Contact Me Here</button>
 
-
+            <img src = {menu} alt="Menu" className='mobMenu' onClick={()=>setShowMenu(!showMenu)}/>
+            <div className="navMenu" style={{display: showMenu? 'flex':'none'}}>
+                <Link activeClass='active' to='intro' spy={true} smooth={true} offset={-100} duration={500} className="ListItem" onClick={()=>setShowMenu(false)}>Home</Link>
+                <Link activeClass='active' to='skills' spy={true} smooth={true} offset={-100} duration={500} className="ListItem" onClick={()=>setShowMenu(false)}>About</Link>
+                <Link activeClass='active' to='contact' spy={true} smooth={true} offset={-100} duration={500} className="ListItem" onClick={()=>setShowMenu(false)}>Portfolio</Link>
+                <Link activeClass='active' to='contact' spy={true} smooth={true} offset={-100} duration={500}  className="ListItem" onClick={()=>setShowMenu(false)}>Message Me</Link>
+            </div>
         </nav>
     )
 }
